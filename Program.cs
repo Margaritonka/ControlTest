f7d12ae8e4fd528b98b22aa228126d0ecb83b55f
Margarita <bano4kaa@mail.ru>
2022-10-01 23:23:33 +0300

    add count cycle

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+int CountArraySize(string[] first)
+{
+    int sum = 0;
+    for (int i = 0; i < first.Length; i++)
+    {
+        if (first[i].Length < 4)
+        {
+            sum += 1;
+        }
+    }
+    return sum;
+}
+
+
+string[] first = { "Sam", "Alexander", "Tom", "Margarita", "Bob", "Ror" };
+string[] second = new string[CountArraySize(first)];
