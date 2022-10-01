e7ea611ce2d84d24a6ee681e5477c07bdaa2d380
Margarita <bano4kaa@mail.ru>
2022-10-01 23:26:11 +0300

    add CopyElem method

--- Program.cs
+++ Program.cs
@@ -1,4 +1,18 @@
-﻿
+﻿string[] CopyElem(string[] first, string[] second)
+{
+    int j = 0;
+    for (int i = 0; i < first.Length; i++)
+    {
+        if (first[i].Length < 4)
+        {
+            second[j] = first[i];
+            j++;
+        }
+    }
+    return second;
+}
+
+
 int CountArraySize(string[] first)
 {
     int sum = 0;
@@ -15,3 +29,5 @@ int CountArraySize(string[] first)
 
 string[] first = { "Sam", "Alexander", "Tom", "Margarita", "Bob", "Ror" };
 string[] second = new string[CountArraySize(first)];
+
+CopyElem(first, second);
